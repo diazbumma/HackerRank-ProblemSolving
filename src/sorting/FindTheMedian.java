6c132969eaa020602de510297664fbc861ab6552
Odiaz Bumma <odiazbumma@gmail.com>
2019-11-06 15:01:01 +0700

    Sorting: find the median, done

--- src/sorting/FindTheMedian.java
+++ src/sorting/FindTheMedian.java
@@ -9,7 +9,7 @@ public class FindTheMedian {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
             arr[i] = scanner.nextInt();
         System.out.println(findMedian(arr));
     }
