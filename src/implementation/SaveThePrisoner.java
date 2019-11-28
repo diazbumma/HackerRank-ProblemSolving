62384736d31f632c99d942a0441c95ef618cdaab
Odiaz Bumma <odiazbumma@gmail.com>
2019-11-28 10:35:55 +0700

    Implementation: save the prisoner, done

---
+++ src/implementation/SaveThePrisoner.java
@@ -0,0 +1,30 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class SaveThePrisoner {
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        int remain = m % n;
+        if (remain == 0)
+            return s-1==0 ? n : s-1;
+        int start = s-1;
+        return (start+remain)>n ? (start+remain)-n : (start+remain);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nms = scanner.nextLine().split(" ");
+            int n = Integer.parseInt(nms[0]);
+            int m = Integer.parseInt(nms[1]);
+            int s = Integer.parseInt(nms[2]);
+            int result = saveThePrisoner(n, m, s);
+            System.out.println(result);
+        }
+        scanner.close();
+    }
+}
