62b5b54c434505d056937fddac1148efe4437f90
Odiaz Bumma <odiazbumma@gmail.com>
2019-12-09 20:12:35 +0700

    Trees: binary search tree insertion, done

---
+++ src/trees/BinarySearchTreeInsertion.java
@@ -0,0 +1,64 @@
+package trees;
+
+import java.util.Scanner;
+
+public class BinarySearchTreeInsertion {
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static void preOrder(Node root) {
+        if(root == null)
+            return;
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null)
+            return new Node(data);
+
+        Node current =  root;
+        boolean inserted = false;
+        while (!inserted) {
+            if (data > current.data) {
+                if (current.right == null) {
+                    current.right = new Node(data);
+                    inserted = true;
+                } else {
+                    current = current.right;
+                }
+            } else {
+                if (current.left == null) {
+                    current.left = new Node(data);
+                    inserted = true;
+                } else {
+                    current = current.left;
+                }
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }
+}
